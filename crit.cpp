01db3233dfc612127ee3635e7eb5a05b87114899
Gregory Ridgway <gregridgway@gmail.com>
2019-07-03 11:09:27 -0400

    got rid of some extraneous checks

--- crit.cpp
+++ crit.cpp
@@ -196,7 +196,7 @@ void crit_dp(double &pplus, double &dpdr, double *result, double mye, int s)
 	double eeoT4 = mye/T(s)/T(s)/T(s)/T(s), ppoT4 = getint_poT4(), ccs2 = cs2(mye); //!!!!!
 	double woT4 = eeoT4 + ppoT4;
 
-  double logmeasure_o2T3, measure_o2=0, q=0, q_sq=0, Yi=0, Xi=0, wplus_oT4=0, tmp=0, sign=1;
+  double measure_o2=0, q=0, q_sq=0, Yi=0, Xi=0, wplus_oT4=0, tmp=0, sign=1;
 	double dpde_sum=0, dpde, dpdlogphi;
   double ds=0, db=0;
   double A_ = getint_A(), Atp_ = getint_Atp(s), Atpp_ = getint_Atpp(s);
@@ -211,18 +211,9 @@ void crit_dp(double &pplus, double &dpdr, double *result, double mye, int s)
 		Yi = phi[i][s]/phi_eq - 1;
 		phi_dot  = -Atp_/(1+q_sq);														   			 // w * d log phi_eq / de
 		phi_2dot = -Atpp_/(1+q_sq) + Atp_*Atp_/(1+q_sq)/(1+q_sq);      // w^2 * d^2 log phi_eq / de^2
-		//cout << i << "\t" << q << "\t" << Yi << "\t" << phi_dot << "\t" << phi_2dot << endl;
-    //logmeasure_o2T3 = log(q*dQ[i]/(2*M_PI*M_PI)/2) - 3*log(T(s)); // log of integration measure divided by 2 T^3
 		measure_o2 = q*dQ[i]/(2*M_PI*M_PI)/2;
 		ds           +=  measure_o2 * (log(1+Yi) - Yi);
-		//if((i == 119 or i == 118) and s==1) 
-		if(Yi+1 <= 0)
-		{
-			cout << t*.1973*A << "\t" << i << "\t" << s << "\t" << phi[i][s] << "\t" << phi_eq << endl;
-			abort();
-		}
 		db           +=  measure_o2 * phi_dot * Yi / w;
-		// integral in expression for dp/de, to be multiplied by w(+)/beta(+)w
 		dpde_sum     +=  measure_o2 * (Yi*phi_2dot + (1-Yi) * phi_dot*phi_dot);
 
 		tmp           = -measure_o2 * phi_dot*(1-Yi) / phi[i][s];
@@ -232,37 +223,11 @@ void crit_dp(double &pplus, double &dpdr, double *result, double mye, int s)
 		result[3]    += measure_o2 * Yi * Dtphi(i, s, phi_eq)/phi[i][s];
 		dpdr         += measure_o2 * Yi * Drphi(i, s)/phi[i][s];
 
-		/*
-		//To avoid overflow
-		tmp = log(Xi) - Yi;
-		if(tmp != 0)
-		{
-			tmp = logmeasure_o2T3 + log(-tmp);
-			if(tmp > -30) Tm3ds -= exp(tmp);
-		}
-		tmp = phi_p*Yi;
-		if(tmp != 0)
-		{
-			sign = tmp/abs(tmp);
-			tmp = logmeasure_o2T3 + log(sign*tmp);
-			if(tmp>-30) Tdb += sign*exp(tmp);
-		}
-		tmp = phi_pp*Yi + phi_p*phi_p*(1-Yi);
-		//tmp = Yi * (phi_pp * woT4 + phi_p);
-		if(tmp!=0)
-		{
-			sign = tmp/abs(tmp); 
-			tmp = logmeasure_o2T3 + log(sign*tmp);
-			if(tmp>-30) dpde_sum += sign*exp(tmp);
-		}
-		*/
   }
 	double Tbplus = (1 + myT*db);
 	double dp    = myT*(ds - w*db)/Tbplus;
 	       pplus = myp + dp;
 	double wplus = mye + pplus;
-
-	//if(t*.1973*A > 4.157 and isnan(dp)) cout << s << "\t" << Tbplus << "\t" << ds << "\t" << db << "\t" << myT <<  endl;
 
 	dpdr      += dpdr_term * wplus/w;
 	dpdr      *= myT/Tbplus;
@@ -273,38 +238,4 @@ void crit_dp(double &pplus, double &dpdr, double *result, double mye, int s)
 	dpde       = ccs2 + myT*wplus/w/w/Tbplus * dpde_sum;
 	result[2]  = dpde;
 	dpdr      += dpde * Dre(s);
-	//cout << s << "\t" << ds << "\t" << db << "\t" << dp << endl;
-	//cout << s << "\t" << mye << "\t" << myT << "\t" << w << "\t" << pplus << "\t"  << Tbplus << "\t" << ccs2 << "\t" << dpde << "\t" << Dre(s) << "\t" << dpdr << endl;
-
-
-	/*
-	wplus_oT4 = (woT4 + Tm3ds)/(1+Tdb);
-	pplus = (wplus_oT4)*T(s)*T(s)*T(s)*T(s) - mye;
-	//if((s>200) && (s<250)) cout << "s, T (GeV), e/T^4, p/T^4, w/T^4, T^-3 ds, T db: " << s << "\t" << T(s)/A << "\t" << eeoT4 << "\t" << ppoT4 << "\t" << woT4 << "\t" << Tm3ds << "\t" << Tdb << endl;
-	if(isnan(pplus)){cout << s*.1973*A << " pplus is nan " << endl; abort();}
-	dpde = wplus_oT4/woT4/(1+Tdb)*(ccs2 - woT4 * dpde_sum);
-	//dpde = wplus_oT4/woT4/(1+Tdb)*(ccs2 - dpde_sum);
-	result[2] = dpde;
-	//cout << s << "\t" << ccs2 << "\t" << dpde_sum << "\t" << phi_p*phi_p << endl;
-
-	dpdr = dpde * Dre(s);
-	result[3]=0;
-	
-  for(int i = 0; i<NUM_MODES; ++i)
-	{
-		q = Q[i]*Q[i];
-		q_sq = q/A_;
-		phi_eq = 1/(q + A_);
-		Yi = phi[i][s]/phi_eq - 1;
-		phi_p = Atp_/(1+q_sq);
-    measure = q*dQ[i]/(2*M_PI*M_PI);
-
-		dpdlogphi = -.5*T(s)*measure/(1+Tdb) * (Xi*phi_p*wplus_oT4 + Yi);
-		dpdr += dpdlogphi * (Drphi(i,s)/phi[i][s]); // !!!!! perhaps make a d log(phi) / dr and d log(phi) / dt
-		result[3] += dpdlogphi * Dtphi(i,s,phi_eq)/phi[i][s];
-		//cout << s << "\t" << dpdlogphi << "\t" << Drphi(i,s)/phi[i][s] << endl;
-	}
-	//cout << s << "\t" << dpde << "\t" << dpdlogphi << "\t" << dpdr << "\t" << result[3] << endl;
-	*/
-	
 }
